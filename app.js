56618558a785ea7799b74aa64babf03aa91248c6
Ehiksalex <ehiksalex@gmail.com>
2022-04-28 06:59:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var bmiCal = document.getElementById("bmiCal");
+
+function bmiCalculator(weight,height){
+    var kg = weight;
+    var m = height;
+    var bmi = kg/(m**2);
+    var BMI = bmi.toFixed(2)
+    if (BMI < 18.5){
+        return "Your BMI is "+ BMI +"," + " so you are underweight."
+    }
+    else if (BMI> 18.5 && BMI <= 24.9){
+        return "Your BMI is "+ BMI +"," + " so you have a normal weight."
+    }
+    else if(BMI > 24.9){
+        return "Your BMI is "+ BMI +"," + " so you are overweight."
+    }
+        
+}
+var result =  bmiCalculator(65,1.8);
+bmiCal.innerHTML = result;
+
